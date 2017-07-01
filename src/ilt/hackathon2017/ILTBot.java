44074a1cbb8d3b8594c36bd9f5f7f0b2e421a8ec
Richard Stapenhurst <richard.stapenhurst@gmail.com>
2017-07-01 15:00:32 +0100

    printit

--- src/ilt/hackathon2017/ILTBot.java
+++ src/ilt/hackathon2017/ILTBot.java
@@ -1,8 +1,9 @@
 package ilt.hackathon2017;
 
-import org.telegram.telegrambots.api.methods.BotApiMethod;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class ILTBot extends TelegramLongPollingBot {
 
@@ -20,6 +21,15 @@ public class ILTBot extends TelegramLongPollingBot {
 	@Override
 	public void onUpdateReceived(Update update) {
 		System.out.println(update);
+		SendMessage s = new SendMessage();
+		s.setText("hello, you said: " + update.getMessage().getText());
+		s.setChatId(update.getMessage().getChatId());
+		try {
+			sendMessage(s);
+		} catch (TelegramApiException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
