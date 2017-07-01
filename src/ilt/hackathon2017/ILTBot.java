2d44608f5115699899071cad75ae7fb7ad48d4c1
Michael Worcester <worcesterm@gmail.com>
2017-07-01 14:41:05 +0100

    Added ILTBot

--- src/ilt/hackathon2017/ILTBot.java
+++ src/ilt/hackathon2017/ILTBot.java
@@ -1,5 +1,30 @@
 package ilt.hackathon2017;
 
-public class ILTBot {
+import org.telegram.telegrambots.api.methods.BotApiMethod;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
 
+public class ILTBot extends TelegramWebhookBot {
+
+	@Override
+	public String getBotPath() {
+		return null;
+	}
+
+	@Override
+	public String getBotUsername() {
+		return "DigitalLegalAssitantBot";
+	}
+
+	@Override
+	public BotApiMethod onWebhookUpdateReceived(Update update) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getBotToken() {
+		return "448560174:AAGQB_MdSfaIPFG67QTuE0kSArvJd_ybkL8";
+	}
+	
 }
