842a53f8211bd223880522ea198a6028e7564b7e
Richard Stapenhurst <richard.stapenhurst@gmail.com>
2017-07-01 14:45:42 +0100

    logging

--- src/ilt/hackathon2017/Main.java
+++ src/ilt/hackathon2017/Main.java
@@ -1,14 +1,26 @@
 package ilt.hackathon2017;
 
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+
 import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.logging.BotLogger;
+import org.telegram.telegrambots.logging.BotsFileHandler;
 
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
+		BotLogger.setLevel(Level.ALL);
+        BotLogger.registerLogger(new ConsoleHandler());
+        try {
+            BotLogger.registerLogger(new BotsFileHandler());
+        } catch (IOException e) {
+            BotLogger.severe("TAG:", e);
+        }
 		System.out.println("Hello");
 		TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
 		telegramBotsApi.registerBot(new ILTBot());
-		
 	}
 
 }
